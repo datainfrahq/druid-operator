67805028f2df01d48cb60ef1f47caa5ebb2e4950
gurjotkaur20 <gurjotkaur20@gmail.com>
2023-05-04 14:33:44 +0530

    fix: test cases added to check replicaCount and updating replicaCount

--- controllers/druid/druid_controller_test.go
+++ controllers/druid/druid_controller_test.go
@@ -7,6 +7,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/types"
 
 	druidv1alpha1 "github.com/datainfrahq/druid-operator/apis/druid/v1alpha1"
@@ -25,6 +26,7 @@ var _ = Describe("Druid Operator", func() {
 		timeout  = time.Second * 45
 		interval = time.Millisecond * 250
 	)
+	druid := &druidv1alpha1.Druid{}
 
 	Context("When testing Druid Operator", func() {
 		druidCR, err := readDruidClusterSpecFromFile(filePath)
@@ -35,8 +37,6 @@ var _ = Describe("Druid Operator", func() {
 			Expect(k8sClient.Create(context.TODO(), druidCR)).To(Succeed())
 
 			// Get CR and match ConfigMaps
-			druid := &druidv1alpha1.Druid{}
-
 			expectedConfigMaps := []string{
 				fmt.Sprintf("druid-%s-brokers-config", druidCR.Name),
 				fmt.Sprintf("druid-%s-coordinators-config", druidCR.Name),
@@ -106,6 +106,88 @@ var _ = Describe("Druid Operator", func() {
 			Expect(druid.Status.Ingress).Should(ConsistOf(expectedIngress))
 
 		})
+
+		It("Test broker deployment", func() {
+			createdDeploy := &appsv1.Deployment{}
+			brokerDeployment := fmt.Sprintf("druid-%s-brokers", druidCR.Name)
+			depNamespacedName := types.NamespacedName{Name: brokerDeployment, Namespace: druidCR.Namespace}
+
+			// Match Deployment replicas
+			By("Get deployment and check replicas")
+
+			Eventually(func() bool {
+				err := k8sClient.Get(context.TODO(), depNamespacedName, createdDeploy)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			Expect(*createdDeploy.Spec.Replicas).To(Equal(druidCR.Spec.Nodes["brokers"].Replicas))
+
+			Eventually(func() bool {
+				err := k8sClient.Get(context.TODO(), types.NamespacedName{Name: druidCR.Name, Namespace: druidCR.Namespace}, druid)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			By("Update broker deployment replicas")
+			replicaCount := 2
+			if druidRep, ok := druid.Spec.Nodes["brokers"]; ok {
+				druidRep.Replicas = int32(replicaCount)
+				druid.Spec.Nodes["brokers"] = druidRep
+			}
+			// updating CR
+			Expect(k8sClient.Update(context.TODO(), druid)).Should(Succeed())
+
+			// Fetch druid CR and check replicas
+			Eventually(func() bool {
+				k8sClient.Get(context.TODO(), depNamespacedName, druid)
+				return druid.Spec.Nodes["brokers"].Replicas == 2
+			}, timeout, interval).Should(BeTrue())
+
+			// Fetch deployment and check replicas
+			Eventually(func() bool {
+				k8sClient.Get(context.TODO(), depNamespacedName, createdDeploy)
+				return *createdDeploy.Spec.Replicas == 2
+			}, timeout, interval).Should(BeTrue())
+		})
+
+		// Test statefulsets replica count and update the replica count then match
+		expectedStatefulSets := []string{"coordinators", "historicals", "routers"}
+		for _, names := range expectedStatefulSets {
+			names := names
+			It(fmt.Sprintf("Statefulset test for %s", names), func() {
+				createdSts := &appsv1.StatefulSet{}
+				stsName := fmt.Sprintf("druid-%s-%s", druidCR.Name, names)
+				stsNamespacedName := types.NamespacedName{Name: stsName, Namespace: druidCR.Namespace}
+
+				// Match statefulset replicas
+				By(fmt.Sprintf("Get statefulset and check replicas for %s ", stsName))
+				Eventually(func() bool {
+					err := k8sClient.Get(context.TODO(), stsNamespacedName, createdSts)
+					return err == nil
+				}, timeout, interval).Should(BeTrue())
+				Expect(*createdSts.Spec.Replicas).To(Equal(druidCR.Spec.Nodes[names].Replicas))
+
+				By(fmt.Sprintf("Update statefulset replicas %s ", stsName))
+				replicaCount := 2
+				if druidRep, ok := druid.Spec.Nodes[names]; ok {
+					druidRep.Replicas = int32(replicaCount)
+					druid.Spec.Nodes[names] = druidRep
+				}
+				// updating CR
+				Expect(k8sClient.Update(context.TODO(), druid)).Should(Succeed())
+
+				// Fetch druid CR and check replicas
+				Eventually(func() bool {
+					k8sClient.Get(context.TODO(), types.NamespacedName{Name: druidCR.Name, Namespace: druidCR.Namespace}, druid)
+					return druid.Spec.Nodes[names].Replicas == 2
+				}, timeout, interval).Should(BeTrue())
+
+				// Fetch statefulset and check replicas
+				Eventually(func() bool {
+					k8sClient.Get(context.TODO(), stsNamespacedName, createdSts)
+					return *createdSts.Spec.Replicas == 2
+				}, timeout, interval).Should(BeTrue())
+			})
+		}
 	})
 })
 
