93138e2d27b0af835528054cf08ba730fad5c227
Itamar Marom <46691031+itamar-marom@users.noreply.github.com>
2023-07-26 11:22:06 +0300

    Update apis/druid/v1alpha1/druid_types.go
    Co-authored-by: Cyril Corbon <corboncyril@gmail.com>

--- apis/druid/v1alpha1/druid_types.go
+++ apis/druid/v1alpha1/druid_types.go
@@ -254,6 +254,8 @@ type DruidSpec struct {
 	DeepStorage *DeepStorageSpec `json:"deepStorage,omitempty"`
 
 	// DimensionsMapPath Custom Dimension Map Path for statsd emitter.
+	// stastd documentation is described in the following documentation: 
+	// https://druid.apache.org/docs/latest/development/extensions-contrib/statsd.html
 	// +optional
 	DimensionsMapPath string `json:"metricDimensions.json,omitempty"`
 
