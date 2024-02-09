ca15f47abab33c5087a2ca1a98be2a62a04d4ba1
AdheipSingh <adheip1222@gmail.com>
2024-02-10 00:18:10 +0530

    e2e increase timeout

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -2,7 +2,7 @@
 set -o errexit
 set -x
 # Get Kind
-go install sigs.k8s.io/kind@v0.17.0
+go install sigs.k8s.io/kind@v0.21.0
 # minio statefulset name
 MINIO_STS_NAME=myminio-ss-0
 # druid namespace
@@ -45,7 +45,7 @@ kubectl apply -f e2e/configs/druid-cr.yaml -n ${NAMESPACE}
 sleep 10
 for d in $(kubectl get pods -n ${NAMESPACE} -l app=druid -l druid_cr=tiny-cluster -o name)
 do
-  kubectl wait -n ${NAMESPACE} "$d" --for=condition=Ready --timeout=5m
+  kubectl wait -n ${NAMESPACE} "$d" --for=condition=Ready --timeout=15m
 done
 # wait for druid pods
 for s in $(kubectl get sts -n ${NAMESPACE} -l app=${NAMESPACE} -l druid_cr=tiny-cluster -o name)
