09df45e0f069a8f17eed5afebe62b1f4d13dec9f
Jesper Larsson <jesper@lolo.company>
2024-06-07 17:55:17 +0200

    Consolidate type files

--- controllers/lookup/types.go
+++ controllers/lookup/types.go
@@ -2,6 +2,11 @@ package lookup
 
 import "k8s.io/apimachinery/pkg/types"
 
+type LookupKey struct {
+	Tier string
+	Id   string
+}
+
 type Spec struct {
 	name types.NamespacedName
 	spec interface{}
